badc89b85b087843c22babad763f251a5a0b0a99
ChrisCodeSmith <67827118+ChrisCodeSmith@users.noreply.github.com>
2020-12-09 07:18:47 +0100

    Update fibonacci.py
    Add tabulation

--- fibonacci.py
+++ fibonacci.py
@@ -29,6 +29,16 @@ def fib_memo(n, memo=None):
 # see here: https://florimond.dev/blog/articles/2018/08/python-mutable-defaults-are-the-source-of-all-evil/
 
 
+# using tabulation to reduce complexity to O(n) for iterative calculation
+def fib_tab(n: int):
+    tab = [0]*(n+2)
+    tab[1] = 1
+    for x in range(0, n):
+        tab[x+1] += tab[x]
+        tab[x+2] += tab[x]
+    return tab[n]
+
+
 # using python builtin functools module to implement memoization.
 @functools.cache
 def fib_cached(n):
@@ -40,16 +50,25 @@ def fib_cached(n):
 # The Fibonacci number to calculate in all 3 algorithms
 fib_to_calc = 37
 
+print('\nPrimitive Fib Calc:')
 start = time.time()
 print(fib(fib_to_calc))
 end = time.time()
 print(f"Time taken: {end-start}")
 
+print('\nMemoization Fib Calc:')
 start = time.time()
 print(fib_memo(fib_to_calc))
 end = time.time()
 print(f"Time taken: {end-start}")
 
+print('\nTabulation Fib Calc:')
+start = time.time()
+print(fib_tab(fib_to_calc))
+end = time.time()
+print(f"Time taken: {end-start}")
+
+print('\nfunctools.cache Fib Calc:')
 start = time.time()
 print(fib_cached(fib_to_calc))
 end = time.time()
